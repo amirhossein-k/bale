ccb48d19ec7dfaf3eb4b85c87a5ac420554d5cfd
amirhossein-k <amirhossein.k787878@gmail.com>
2026-04-29 15:12:08 +0330

    efefe

--- src/app/telegram/handlers/start.js
+++
@@ -1,8 +0,0 @@
-export function startHandler() {
-  return async (ctx) => {
-    // پیام خوش‌آمد + درخواست اسم (در caption)
-    await ctx.reply(
-      "👋 خوش آمدی! بیا پروفایلت رو بسازیم.\n\n📌 مرحله ۱ از ۵: لطفاً اسمت رو ارسال کن.",
-    );
-  };
-}
